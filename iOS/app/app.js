b0d9617e916c4922122ee9b3782605ddd007c96a
Troy Dawson <klocwise@comcast.net>
2013-02-10 16:00:32 -0800

    monkeying with js app stuff

--- iOS/app/app.js
+++ iOS/app/app.js
@@ -1,5 +1,31 @@
 var app = {
 
+	touch: function(e) {
+		
+		if (app.box.style.zIndex < 1) {
+			$(app.box).css({ zIndex: 1, backgroundColor: '#FF00FF' }).appendTo('body');
+		}
+		
+		if (typeof e === 'undefined')
+			return;
+		
+		var touch = e.touches[0];
+		
+		app.box.style.left = touch.pageX+'px';
+		app.box.style.top = touch.pageY+'px';
+	},
+	
+	down: function(e) {
+	
+		$('.match').animate({ marginTop: '100px'});
+	},
+	
+	up: function(e) {
+		
+		$('.match').animate({ marginTop: '0px'});
+	},
+	
+
 	deviceready: function() {
 
 		console.log('hello');
@@ -15,10 +41,25 @@ var app = {
 		
 		ctx.fillStyle = '#DEDEC0';
 
-		ctx.fillRect(0,0,app_canvas.width, app_canvas.height);
+		ctx.fillRect(0, 0, app_canvas.width, app_canvas.height);
 
 		document.body.appendChild(app_canvas);
+
+		app.box = document.createElement('div');
+		app.box.className = 'box';
+		app.box.style.zIndex = 0;
+		
+		for (var i = 0; i < 12; i++)
+			$('<div/>').addClass('match').css({ top: (Math.floor(i/3)*48)+'px', left: (2+(i%3)*106)+'px' }).appendTo('body')
+
+		document.addEventListener('touchstart', function(e) { app.touch(e) }, false);
+		document.addEventListener('touchmove', function(e) { app.touch(e) }, false);
+
+		document.addEventListener('touchstart', function(e) { app.down(e) }, false);
+		document.addEventListener('touchend', function(e) { app.up(e) }, false);
+
 	}
+	
 };
 
 
