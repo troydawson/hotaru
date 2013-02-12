f696536458a6482a0d945c91774216fe3f751848
troydawson <troy-dawson@comcast.net>
2013-02-11 23:27:59 -0800

    initial commit of vs side

---
+++ VS/hotaru/app/app.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var el = document.getElementById('content');
+};
+//@ sourceMappingURL=app.js.map
---
+++ VS/hotaru/app/app.ts
@@ -0,0 +1,3 @@
+window.onload = () => {
+    var el = document.getElementById('content');
+};
