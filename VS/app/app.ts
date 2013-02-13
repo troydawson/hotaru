da1c4a0fc8b60cbf52e1e45f3a391500223c4c37
troydawson <troy-dawson@comcast.net>
2013-02-12 21:48:10 -0800

    got blockboard rendering

--- VS/app/app.ts
+++ VS/app/app.ts
@@ -1,10 +1,79 @@
-/// <reference path="../def/underscore.d.ts" />
 /// <reference path="../def/state-machine.d.ts" />
+/// <reference path="../def/three.d.ts" />
 /// <reference path="../def/jquery.d.ts" />
+/// <reference path="../def/underscore.d.ts" />
 /// <reference path="../def/mousetrap.d.ts" />
 /// <reference path="../def/toastr.d.ts" />
 
+declare var cordova;
+var on_device = typeof cordova != 'undefined';
+
 //? var app;
+
+class CGPoint extends THREE.Vector2{
+	constructor(x?: number = 0, y?: number = 0) { super(x, y) }
+}
+
+var CGPointZero = new CGPoint();
+
+class CGSize {
+	constructor(public width?: number = 0, public height?: number = 0) { }
+}
+
+class CGRect {
+	constructor(public origin?: CGPoint = CGPointZero, public size?: CGSize = new CGSize()) { }
+}
+
+class BlockBoardElement {
+
+	src_frame: CGRect;
+	dst_frame: CGRect;
+	alpha: number = 0.55;
+
+	constructor(i: number, public id: string) {
+
+		var row = Math.floor(i / 5);
+		var col = i % 5;
+
+		this.src_frame = new CGRect(new CGPoint(4 + i * (96 + 8), 4), new CGSize(96, 96));
+
+		this.dst_frame = new CGRect(new CGPoint(24 + col * (48 + 8), 460 - (2 - row) * (48 + 8)), new CGSize(48, 48));
+	}
+
+	render(ctx: CanvasRenderingContext2D, src_image: HTMLImageElement) {
+		
+		var previous_alpha = ctx.globalAlpha;
+
+		ctx.globalAlpha = this.alpha;
+
+		ctx.drawImage(src_image, this.src_frame.origin.x, this.src_frame.origin.y, this.src_frame.size.width, this.src_frame.size.height,
+			this.dst_frame.origin.x, this.dst_frame.origin.y, this.dst_frame.size.width, this.dst_frame.size.height);
+
+		ctx.globalAlpha = previous_alpha;
+	}
+}
+
+class BlockBoard {
+
+	blocks: BlockBoardElement[] = [];
+
+	constructor(public ctx: CanvasRenderingContext2D, public ui_image: HTMLImageElement) {
+
+	var block_ids = "0123456789";
+
+	for (var i = 0; i < 10; i++)
+		this.blocks.push(new BlockBoardElement(i, block_ids.charAt(i)));
+
+	}
+
+	render() {
+
+		for (var i = 0; i < 10; i++) {
+			this.blocks[i].render(this.ctx, this.ui_image);
+		}
+	}
+}
+
 
 class App {
 
@@ -28,6 +97,8 @@ class App {
 
 	}
 
+	ctx: CanvasRenderingContext2D;
+
 	InitCanvas() {
 
 		$(document.createElement('div')).attr('id', 'app_container').appendTo('body');
@@ -36,14 +107,14 @@ class App {
 
 		app_canvas.id = 'app_canvas';
 
-		app_canvas.width = 320;  //window.innerWidth;
-		app_canvas.height = 480;  //window.innerHeight;
+		app_canvas.width = on_device ? window.innerWidth : 320;
+		app_canvas.height = on_device ? window.innerHeight : 460;
 
-		var ctx = app_canvas.getContext('2d');
+		this.ctx = app_canvas.getContext('2d');
 
-		ctx.fillStyle = '#DEDEC0';
+		this.ctx.fillStyle = '#DEDEC0';
 
-		ctx.fillRect(0, 0, app_canvas.width, app_canvas.height);
+		this.ctx.fillRect(0, 0, app_canvas.width, app_canvas.height);
 
 		$(app_canvas).appendTo('#app_container');
 	}
@@ -68,26 +139,39 @@ class App {
 					});
 	}
 
+	blockboard: BlockBoard;
+
+	CreateUI(ui_image: HTMLImageElement) {
+		this.blockboard = new BlockBoard(this.ctx, ui_image);
+		this.blockboard.render();
+	}
+
 	KanjibotInit() {
 
 		this.InitCanvas();
 
 		this.AddSwiper();
 
+		var ui_image = new Image();
+
+		this.ctx.globalAlpha = 0.6;
+
+		ui_image.onload = () => this.CreateUI(ui_image);
+		ui_image.onerror = () => toastr.error('not loaded!');
+
+		ui_image.src = 'img/ui_elements@2x.png';
 	}
 
+
 	WindowsInit() {
-		Mousetrap.bind('4', () => { console.log('4') });
+
+		Mousetrap.bind('4', () => console.log('4'));
 
 		this.KanjibotInit();
-
-		toastr.success('Windows init OK');
 	}
 }
 
-declare var cordova;
-
-if (typeof cordova != 'undefined')
+if (on_device)
 	document.addEventListener('deviceready', () => new App().KanjibotInit(), false);
 else
 	window.onload = () => new App().WindowsInit();
